d7b6a246446ecda1d6fd52230f1477029de27c57
ayeleta <ayelet.a@qualisystems.com>
2016-05-24 12:32:16 -0700

    Fix code review issues
    _GetRootResources - actually find the parents
    Don't cache root resources
    activate routes: don't use activatetopology
    pep 8 method names

--- EnvironmentCommands/Setup.py
+++ EnvironmentCommands/Setup.py
@@ -1,32 +1,40 @@
 from QualiEnvironmentUtils.Sandbox import *
 from QualiEnvironmentUtils.Networking.NetworkingSaveNRestore import *
 
-dev.attach_to_cloudshell_as('admin', 'admin', 'Global', '18e5dd1f-aa63-493c-a72c-aa1809f4cb17',
+dev.attach_to_cloudshell_as('admin', 'admin', 'Global', '2385544d-12c5-4667-9a1c-2d213b59fe03',
                             server_address='localhost', cloudshell_api_port='8029')
 
 # ----------------------------------
 # Setup
 # ----------------------------------
-reservation_id=helpers.get_reservation_context_details().id
+reservation_id = helpers.get_reservation_context_details().id
 logger = get_qs_logger(log_category='EnvironmentCommands',
                        log_group=reservation_id, log_file_prefix='Setup')
 
 sandbox = SandboxBase(reservation_id, logger)
-saveNRestoreTool = NetworkingSaveRestore(sandbox)
+do_save_restore = True
+try:
+    saveNRestoreTool = NetworkingSaveRestore(sandbox)
+except QualiError:
+    do_save_restore = False
 try:
     sandbox.clear_all_resources_live_status()
-    if saveNRestoreTool.IsSnapshot():
-        saveNRestoreTool.LoadConfig(config_stage='Snapshots',config_type= 'Running',ignore_models=['Generic TFTP server'])
-    else:
-        saveNRestoreTool.LoadConfig(config_stage='Gold', config_type= 'Running',ignore_models=['Generic TFTP server'])
-    # call activateRoutes
-    sandbox.ActivateRoutes()
+    if do_save_restore:
+        # TODO- Get the config set name from the orchestration's params
+        config_set_name = ''
+        if saveNRestoreTool.is_snapshot():
+            saveNRestoreTool.load_config(config_stage='Snapshots', config_type='Running',
+                                         ignore_models=['Generic TFTP server'])
+        else:
+            saveNRestoreTool.load_config(config_stage='Gold', config_type='Running',
+                                         ignore_models=['Generic TFTP server'], config_set_name=config_set_name)
 
-    # Call RoutesValidation
- #   sandbox.RoutesValidation()
+    # call activate_all_routes_and_connectors
+    sandbox.activate_all_routes_and_connectors()
+
+    # Call routes_validation
+    #   sandbox.routes_validation()
 except QualiError as qe:
     logger.error("Setup failed. " + str(qe))
 except:
-    logger.error("Setup failed. Unexpected error:" + sys.exc_info()[0])
-
-
+    logger.error("Setup failed. Unexpected error:" + str(sys.exc_info()[0]))
--- QualiEnvironmentUtils/Networking/NetworkingSaveNRestore.py
+++ QualiEnvironmentUtils/Networking/NetworkingSaveNRestore.py
@@ -9,21 +9,21 @@ class NetworkingSaveRestore():
         :param SandboxBase sandbox:  The sandbox save & restore will be done in
         """
         self.sandbox = sandbox
-        for resource in self.sandbox.root_resources:
+        root_resources = self.sandbox.get_root_resources()
+        for resource in root_resources:
                 if resource.model == 'Generic TFTP server':
                     try:
-                        tftp_server_destination_path = resource.GetAttribute("TFTP Network configs path")
+                        tftp_server_destination_path = resource.get_attribute("TFTP Network configs path")
                     except:
                         continue
                     if tftp_server_destination_path !="":
                         self.config_files_root = 'tftp://' + resource.address + "/" + tftp_server_destination_path
                         break
         if tftp_server_destination_path =="":
-            self.sandbox.ReportError("Failed to find the network's tftp path",raise_error=True,writeToOutputWindow=True)
 
 
     # ----------------------------------
-    # LoadNetworkConfig(ResourceName,config_type, RestoreMethod=Override)
+    # load_network_config(ResourceName,config_type, RestoreMethod=Override)
     # ResourceName - The name of the resource we would like to load the config onto
     # ConfigPath – the path to the configuration file, including the configuration file name.
     # The path should include the protocol type. This input is mandatory.
@@ -38,7 +38,7 @@ class NetworkingSaveRestore():
     # Base config is an exception. The blueprint's name is not in the path
     # e.g. tftp://configs/Base/svl290-gg07-sw1_c3850.cfg
     # ----------------------------------
-    def LoadConfig(self, config_stage, config_type, restore_method="Override", ignore_models = [], write_to_output = True):
+    def load_config(self, config_stage, config_type, restore_method="Override", config_set_name = '', ignore_models = [], write_to_output = True):
         """
         Load the configuration from config files on the Blueprint's devices
         :param str config_stage:  The stage of the config e.g Gold, Base
@@ -46,13 +46,16 @@ class NetworkingSaveRestore():
         :param str restore_method: Optional. Restore method. Can be Append or Override
         :param list[str] ignore_models: Optional. Model that the function should ignore and not load config on the device
         """
-        self.sandbox.ReloadDetails()
+
         root_path=''
         if config_stage.lower() == 'gold' or config_stage.lower() == 'snapshot':
             root_path =self.config_files_root + '/' + config_stage + '/' + self.sandbox.Blueprint_name + '/'
+            if config_set_name != '':
+                root_path = root_path + config_set_name + '/'
         elif config_stage.lower() == 'base':
             root_path =self.config_files_root + '/' + config_stage + '/'
-        for resource in self.sandbox.root_resources:
+        root_resources = self.sandbox.get_root_resources()
+        for resource in root_resources:
             load_config_to_device = True
             for ignore_model in ignore_models:
                 if resource.model.lower() == ignore_model.lower():
@@ -61,35 +64,33 @@ class NetworkingSaveRestore():
             if load_config_to_device == True:
                 try:
                     config_path = root_path + resource.name + '_' + resource.model + '.cfg'
-                    self.sandbox.ReportInfo('Loading configuration for device: ' + resource.name +
-                                                                     ' from:' + config_path,write_to_output)
-                    resource.LoadNetworkConfig(self.sandbox.id, config_path, config_type, restore_method)
+                    self.sandbox.report_info('Loading configuration for device: ' + resource.name +
+                                                                     ' from:' + config_path, write_to_output)
+                    resource.load_network_config(self.sandbox.id, config_path, config_type, restore_method)
                     self.sandbox.api_session.SetResourceLiveStatus(resource.full_path,'Online')
                 except QualiError as qe:
-                    err = "Failed to load configuration for device " + resource.name + ". " + qe.__str__()
-                    self.sandbox.ReportError(err,writeToOutputWindow=write_to_output,raise_error=False)
+                    err = "Failed to load configuration for device " + resource.name + ". " + str(qe)
                     self.sandbox.api_session.SetResourceLiveStatus(resource.full_path,'Error')
                 except:
                     err = "Failed to load configuration for device " + resource.name + \
-                          ". Unexpected error: " + sys.exc_info()[0]
-                    self.sandbox.ReportError(err,writeToOutputWindow=write_to_output,raise_error=False)
+                          ". Unexpected error: " + str(sys.exc_info()[0])
                     self.sandbox.api_session.SetResourceLiveStatus(resource.full_path,'Error')
 
 
     # ----------------------------------
     # ----------------------------------
-    def SaveConfig(self, snapshot_name,config_type, ignore_models = [],write_to_output=True):
+    def save_config(self, snapshot_name, config_type, ignore_models = [], write_to_output=True):
         """
         Load the configuration from the devices to the tftp
         :param str snapshot_name:  The name of the snapshot
         :param str config_type:  StartUp or Running
         """
-        self.sandbox.ReloadDetails()
+        root_resources = self.sandbox.get_root_resources()
 
         config_path = self.config_files_root + '/Snapshots/' + snapshot_name
 
         # check - do I need to create the snapshot folder on the tfp server if it doesn't exist?
-        for resource in self.sandbox.root_resources:
+        for resource in root_resources:
             save_config_from_device = True
             for ignore_model in ignore_models:
                 if resource.model.lower() == ignore_model.lower():
@@ -97,23 +98,21 @@ class NetworkingSaveRestore():
                     break
             if save_config_from_device == True:
                 try:
-                    self.sandbox.ReportInfo('Saving configuration for device: ' + resource.name + ' to: ' + config_path, write_to_output)
+                    self.sandbox.report_info('Saving configuration for device: ' + resource.name + ' to: ' + config_path, write_to_output)
                     resource.SaveNetworkConfig(self.sandbox.id, config_path, config_type)
 
                 except QualiError as qe:
-                    err = "Failed to save configuration for device " + resource.name + ". " + qe.__str__()
-                    self.sandbox.ReportError(err,writeToOutputWindow=write_to_output)
+                    err = "Failed to save configuration for device " + resource.name + ". " + str(qe)
                 except:
                     err = "Failed to save configuration for device " + resource.name + \
-                          ". Unexpected error: " + sys.exc_info()[0]
-                    self.sandbox.ReportError(err,writeToOutputWindow=write_to_output)
+                          ". Unexpected error: " + str(sys.exc_info()[0])
 
 
 
     # ----------------------------------
     #Is this Sandbox originates from a snapshot Blueprint?
     # ----------------------------------
-    def IsSnapshot(self):
+    def is_snapshot(self):
         #check if there is a directory with the Blueprint's name under the snapshots dir
         envDir = self.config_files_root + '/Snapshots/' + self.sandbox.Blueprint_name
         return os.path.isdir(envDir)
